20b89f39a3df310c581c1da92500b576a30a0725
Winaldi99 <2381030@unai.edu>
2025-05-02 11:26:33 +0700

    Fix migrations

---
+++ src/migrations/1746159426980-CreateGenreTable.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateGenreTable1746159426980 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE genre (
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                description TEXT,
+                created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  
+                CONSTRAINT fk_user
+                    FOREIGN KEY(user_id) 
+                    REFERENCES Users(id)
+                    ON DELETE CASCADE
+            );
+          `);
+          await queryRunner.query(`
+            ALTER TABLE film
+            ADD CONSTRAINT fk_genre
+            FOREIGN KEY (genre_id)
+            REFERENCES genre(id)
+            ON DELETE SET NULL;
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE film
+            DROP CONSTRAINT IF EXISTS fk_genre;
+        `);
+        
+        await queryRunner.query(`DROP TABLE genre;`);
+    }
+
+}
--- src/migrations/1746159744247-CreateFilmTable.ts
+++ src/migrations/1746159744247-CreateFilmTable.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateFilmTable1745986559054 implements MigrationInterface {
+export class CreateFilmTable1746159744247 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            CREATE TABLE "film" (
+            CREATE TABLE film (
                 id SERIAL PRIMARY KEY,
                 user_id INTEGER NOT NULL,
                 title VARCHAR(255) NOT NULL,
@@ -13,16 +13,17 @@ export class CreateFilmTable1745986559054 implements MigrationInterface {
                 image_url TEXT,
                 created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-
-                CONSTRAINT fk_film_user
-                    FOREIGN KEY(user_id)
-                    REFERENCES "users"(id) -- <<< FIX HERE: Change "Users" to "users" (if it wasn't already)
+  
+                CONSTRAINT fk_user
+                    FOREIGN KEY(user_id) 
+                    REFERENCES Users(id)
                     ON DELETE CASCADE
             );
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE IF EXISTS "film";`);
+        await queryRunner.query(`DROP TABLE film;`)
     }
+
 }
